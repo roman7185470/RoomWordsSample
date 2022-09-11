03535d049629102b52e8295fa972df767b0c4ce0
Roman <lishuk.roman.electronics@gmail.com>
2022-09-11 22:32:03 +1000

    Initial commit

---
+++ app/src/main/java/com/example/roomwordssample/Word.kt
@@ -0,0 +1,10 @@
+package com.example.roomwordssample
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+class Word(
+    @PrimaryKey(autoGenerate = true) val id: String
+)
---
+++ app/src/main/java/com/example/roomwordssample/WordsApplication.kt
@@ -0,0 +1,16 @@
+package com.example.roomwordssample
+
+import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+
+
+class WordsApplication : Application() {
+
+    val applicationScope = CoroutineScope(SupervisorJob())
+
+    val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
+    val repository by lazy { WordRepository(database.wordDao()) }
+}
+
+
